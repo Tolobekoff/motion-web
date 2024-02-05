2a5c7bcff03dd8f77ab4b038fe22c6d8a39314b5
Tolobekoff <rysbektolobekov215@gmail.com>
2024-02-05 09:44:24 +0600

    first commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+       <header>
+        <div className="container">
+            <div className="header">
+                <h1>
+                    sdhfsduilh
+                </h1>
+            </div>
+        </div>
+       </header>
+    );
+};
+
+export default Header;
